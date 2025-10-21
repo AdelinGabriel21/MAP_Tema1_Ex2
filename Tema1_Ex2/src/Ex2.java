4eee27b32cec86fc9c31f9b5e4582dd776335a1f
AdelinGabriel21 <116873929+AdelinGabriel21@users.noreply.github.com>
2025-10-21 11:56:53 +0300

    Max number done

--- Tema1_Ex2/src/Ex2.java
+++ Tema1_Ex2/src/Ex2.java
@@ -1,2 +1,12 @@
 public class Ex2 {
+    public int maxNumber(int[] numbers){
+        int max = 0;
+        for(int num : numbers){
+            if (num > max){
+                max = num;
+            }
+        }
+
+        return max;
+    }
 }
--- Tema1_Ex2/src/Main.java
+++ Tema1_Ex2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = {4, 8, 3, 10, 17};
+        Ex2 ex2 = new Ex2();
 
+        System.out.println("Max number: " + ex2.maxNumber(numbers));
     }
 }
